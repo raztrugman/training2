8dae83848e8703701d0918a1e93e0e1843645201
raztrugman <raz@laylinetech.com>
2019-12-31 11:12:06 +0200

    change to odoo

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,8 +1,8 @@
-from odoo import fields, models
+from odoo.odoo import fields, models
 
 class ACourse(models.Model):
     _name = 'openacademy.course.model'
     
     course_name = fields.Char()
     session_ids = fields.One2many('session.model')
-    course_level = fields.Selection([(1,"Easy"),(2,"Medium"),(3,"Hard")], string="Course level")
+    course_level = fields.Selection([(1,'Easy'),(2,'Medium'),(3,'Hard')], string="Course level")
