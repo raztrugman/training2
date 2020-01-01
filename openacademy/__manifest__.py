63d4f1be22bc52d89e4d91ff0f257e1c146b88ce
raztrugman <raz@laylinetech.com>
2020-01-01 15:00:18 +0200

    course and session views

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -25,6 +25,8 @@
     # always loaded
     'data':        [
         "security/ir.model.access.csv",
+        "views/course_views.xml",
+        "views/session_views.xml",
         "data/openacademy_data.xml",
     ],
     # only loaded in demonstration mode
